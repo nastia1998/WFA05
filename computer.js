efdd50e08000d8ed8065f9c04d13202854703a10
Анастасия Лещенко <leshencko.nastia2013@gmail.com>
2019-09-19 14:42:28 +0300

    added showInfo method

--- computer.js
+++ computer.js
@@ -1,48 +1,68 @@
 class Computer {
   constructor(manufacturer, processor) {
-    this.manufacturer = manufacturer;
-    this.processor = processor;
+    this._manufacturer = manufacturer;
+    this._processor = processor;
   }
 
   get manufacturer() {
-    return this.manufacturer;
+    return this._manufacturer;
   }
   set manufacturer(newManufacturer) {
-    this.manufacturer = newManufacturer;
+    this._manufacturer = newManufacturer;
   }
 
   get processor() {
-    return this.processor;
+    return this._processor;
   }
   set processor(newProcessor) {
-    this.processor = newProcessor;
+    this._processor = newProcessor;
+  }
+
+  showInfo() {
+    return [this.manufacturer, this.processor];
   }
 }
 
 class Ultrabook extends Computer {
   constructor(manufacturer, processor, hardDiskSize) {
     super(manufacturer, processor);
-    this.hardDiskSize = hardDiskSize;
+    this._hardDiskSize = hardDiskSize;
   }
 
   get hardDiskSize() {
-    return this.hardDiskSize;
+    return this._hardDiskSize;
   }
   set hardDiskSize(newHardDiskSize) {
-    this.hardDiskSize = newHardDiskSize;
+    this._hardDiskSize = newHardDiskSize;
+  }
+
+  showInfo() {
+    return [super.showInfo(), this.hardDiskSize];
   }
 }
 
 class ComputingServer extends Computer {
   constructor(manufacturer, processor, numOfProcessorCores) {
     super(manufacturer, processor);
-    this.numOfProcessorCores = numOfProcessorCores;
+    this._numOfProcessorCores = numOfProcessorCores;
   }
 
   get numOfProcessorCores() {
-    return this.numOfProcessorCores;
+    return this._numOfProcessorCores;
   }
   set numOfProcessorCores(newNumOfProcessorCores) {
-    this.numOfProcessorCores = newNumOfProcessorCores;
+    this._numOfProcessorCores = newNumOfProcessorCores;
+  }
+
+  showInfo() {
+    return [super.showInfo(), this.numOfProcessorCores];
   }
 }
+
+let computer = new Computer("HP", "AMD");
+let ultrabook = new Ultrabook("Lenovo", "Intel", 256);
+let computingServer = new ComputingServer("ASUS", "Intel", 4);
+
+console.log(computer.showInfo());
+console.log(ultrabook.showInfo());
+console.log(computingServer.showInfo());
